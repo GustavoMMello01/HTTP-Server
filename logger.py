c7cf136df50208e0bb3456e3d6c3b2617d4bc242
Gustavo <gustavommello03@gmail.com>
2023-09-20 00:44:08 -0300

    Log fixed

--- logger.py
+++ logger.py
@@ -14,6 +14,13 @@ def log_worker():
         log_queue.task_done()
 
 def log_request(client_addr, method, path, response_code):
-    timestamp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    log_entry = f"{timestamp} - {client_addr[0]}:{client_addr[1]} - [{method} {path}] - {response_code}\n"
-    log_queue.put(log_entry)
+    try:
+        timestamp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        log_entry = f"{timestamp} - {client_addr[0]}:{client_addr[1]} - [{method} {path}] - {response_code}\n"
+        
+        print(f"Request: {log_entry}")  
+        with log_lock:
+            with open("log/server_log.txt", "a") as log_file:
+                log_file.write(log_entry)
+    except Exception as e:
+        print(f"Failed to write to log: {e}")  
